d57860d74b0e7425d9d114a19f92be1c3a54e0ae
dmilham <dmilham462@gmail.com>
2016-11-10 13:34:09 -0500

    repaired server.js

--- server.js
+++ server.js
@@ -1,34 +1,30 @@
-
-// Dependencies===================================================
 var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var app = express();
-var PORT = process.env.PORT || 1984;
+var port = 3000;
 
-// BodyParser middleware robust====================================
+// bodyParser middleware robust==================================
 
-
-// creates application/json parser 
+// creates application/json parser=============================== 
 var jsonParser = bodyParser.json()
 
-// creates application/x-www-form-urlencoded parser 
+// creates application/x-www-form-urlencoded parser==============
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
  
-// POST /login gets urlencoded bodies 
+// POST /login gets urlencoded bodies============================ 
 app.post('/login', urlencodedParser, function (req, res) {
   if (!req.body) return res.sendStatus(400)
   res.send('welcome, ' + req.body.username)
 })
  
-// POST /api/users gets JSON bodies 
+// POST /api/users gets JSON bodies============================== 
 app.post('/api/users', jsonParser, function (req, res) {
   if (!req.body) return res.sendStatus(400)
   // create user in req.body 
 })
 
-// Listener========================================================
-
+// Listener======================================================
 app.listen(port, function () {
-	console.log('Listening on PORT ' + port);
+  console.log('Listening on PORT ' + port);
 });
